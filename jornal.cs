34385bc42eb66d4d31aec4f6c6b680d809f82f38
Garyeong Bae <orisumin@gmail.com>
2026-05-14 13:39:01 -0600

    created the Jornal class

---
+++ jornal.cs
@@ -0,0 +1,15 @@
+public class Jornal{
+    //All the entries
+    List<Entry>;
+    //A list of prompt, I think this could go to menu class?
+    List<string> prompts = new List<string>();
+
+    public void saveEntries()
+    {
+        //this will save all the entries from the user
+    }
+    public void displayJornal()
+    {
+        //displays all the entries in the journal
+    }
+}
