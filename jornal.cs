0ff12b6203e92808a0850769b4382eda6a99db43
Garyeong Bae <orisumin@gmail.com>
2026-05-15 12:28:32 -0600

    updated functions in the jornal class

--- jornal.cs
+++ jornal.cs
@@ -1,21 +1,61 @@
 using System;
+using System.IO;
 public class Jornal{
     //All the entries
     List<Entry> _entries;
     
 
-    public void saveEntries(List<Entry> entries)
+    public void saveEntriesToJornal(List<Entry> newEntries)
     {//this will save all the entries from the user
-        //get entries from the menu
-        //
-    
+        _entries = newEntries;
     }
-    public void displayJornal(string jornalName)
+    public void saveJornalAsFile()
     {
-        //displays all the entries in the journal
+        Console.WriteLine("What is the file name?");
+        string filelName = Console.ReadLine();    
+        using (StreamWriter saveJornal = new StreamWriter(filelName))
+        {
+            foreach(Entry entry in _entries)
+            {
+                saveJornal.WriteLine(entry);
+            }
+        }
+        Console.WriteLine($"Your jornal is successfuly saved in {filelName}.");
+        
     }
-    public void loadJornal(string jornalName)
+    public void displayJornal()
     {
+        //displays all jornal contents line by line
+        foreach(Entry entry in _entries)
+        {
+            Console.WriteLine(line);
+        }
+    }
+    public string[] loadJornal()
+    {   
+        bool notLoaded = true;
+        while (notLoaded){
+        try
+        {
         //load a jornal that matches user's input
+        Console.WriteLine("What is the file name?");
+        string filelName = Console.ReadLine();
+        string[] jornal = System.IO.File.ReadAllLines(fileName);
+        notLoaded = false;
+        } 
+        catch (FileNotFoundException)
+        {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"No jornal named {fileName}.");
+                Console.ResetColor();
+        }
+        catch (FileLoadException)
+        {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Failed to load the jornal. Try again.");
+                Console.ResetColor();        
+                }
+        return jornal;
+        }
     }
 }
